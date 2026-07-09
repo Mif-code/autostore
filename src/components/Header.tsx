0e251b90f17a3b5c07b334d48f28122f0ec22d96
Moisés Ivanildo Ferreira <moises.i.ferreira@hotmail.com>
2026-07-09 11:00:09 -0300

    feat: adiciona header e barra de pesquisa

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>AutoStore</h1>
+      <p>Encontre o veículo ideal para você</p>
+    </header>
+  );
+}
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,14 @@
+export default function SearchBar() {
+  return (
+    <section>
+      <input
+        type="text"
+        placeholder="Digite marca, modelo ou versão..."
+      />
+
+      <button>
+        Buscar
+      </button>
+    </section>
+  );
+}
